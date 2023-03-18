dc58a9024a21fe2ecfa8b7ab1148947b40006644
Sithi5 <ma.sithis@directbrico.com>
2023-03-18 13:14:44 +0100

    [fix]: args parsing check host is set or exit +
    also check -c the next arguments is a number.

--- src/args.c
+++ src/args.c
@@ -21,28 +21,45 @@ void parse_args(int argc, char *argv[], t_args *args)
     }
     for (int i = 1; i < argc; i++)
     {
-        if (strcmp(argv[i], "-v") == 0)
+        if (ft_strcmp(argv[i], "-v") == 0)
         {
             args->v_flag = 1;
         }
-        else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc)
+        else if (ft_strcmp(argv[i], "-c") == 0 && i + 1 < argc)
         {
-            args->num_packets = atoi(argv[i + 1]);
-            i++;
+            if (i + 1 < argc && ft_isnumber(argv[i + 1]))
+            {
+                args->num_packets = ft_atoi(argv[i + 1]);
+                i++;
+            }
+            else
+            {
+                fprintf(stderr, "%s: invalid count of packets to transmit: `%s'\n", PROGRAM_NAME, argv[i + 1]);
+                exit(ERROR_ARGS);
+            }
         }
-        else if (strcmp(argv[i], "-h") == 0)
+        else if (ft_strcmp(argv[i], "-h") == 0)
         {
             usage();
         }
-
         else if (args->host == NULL)
         {
             if (argv[i][0] == '-')
             {
-                printf("Invalid option -- %s\n", argv[i]);
+                fprintf(stderr, "%s: Invalid option -- `%s`\n", PROGRAM_NAME, argv[i]);
                 usage();
             }
             args->host = argv[i];
         }
+        else
+        {
+            fprintf(stderr, "%s: Invalid option -- `%s`\n", PROGRAM_NAME, argv[i]);
+            usage();
+        }
+    }
+    if (args->host == NULL)
+    {
+        usage();
+        exit(ERROR_ARGS);
     }
 }
